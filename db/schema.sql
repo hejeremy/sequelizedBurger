766408f57c0e35ec5f10cbd199543b13db14b345
jeremyhe <jeremyhe1@gmail.com>
2017-09-06 18:13:51 -0400

    stuff

--- db/schema.sql
+++ db/schema.sql
@@ -3,13 +3,22 @@ CREATE DATABASE IF NOT EXISTS burger;
 USE burger;
 
 # If the table already exists, remove it before trying to create the table again
-DROP TABLE IF EXISTS burgers;
+DROP TABLE IF EXISTS Burger;
 
 # Create the burgers table
-CREATE TABLE burgers (
+CREATE TABLE Burger (
 	id INT NOT NULL AUTO_INCREMENT,
 	burger_name VARCHAR(255) NOT NULL,
 	devoured BOOL DEFAULT false,
 	date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Customer (
+	id INT NOT NULL AUTO_INCREMENT,
+	customer VARCHAR(255) NOT NULL,
+	BurgerId INT NOT NULL,
+	createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,7 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Burger1', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Burger2', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Burger3', FALSE);
+INSERT INTO Burger (burger_name, devoured) VALUES ('Burger1', FALSE);
+INSERT INTO Burger (burger_name, devoured) VALUES ('Burger2', FALSE);
+INSERT INTO Burger (burger_name, devoured) VALUES ('Burger3', FALSE);
+
+INSERT INTO Customer (customer, BurgerId) VALUES ('Smith', 1);
+INSERT INTO Customer (customer, BurgerId) VALUES ('Landon', 2);
+INSERT INTO Customer (customer, BurgerId) VALUES ('Smith', 3);
